1072433e14d1763b5e7f4e3d77e5f2c7e21e8174
kosekijsx <leandrobordondev@gmail.com>
2023-04-19 14:24:31 -0300

    🎨️ update

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,6 @@ const ContentCard = (props) => {
   );
 };
 
- 
-
 function App() {
   return <ContentCard />;
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <>
       <h1 className="md:text-6xl xs:text-4xl font-bold underline">
-        <span className="text-primary">L</span>eandro
+        <span className="text-primary">L</span>eandro{" "}
         <span className="text-primary">B</span>ordon
       </h1>
       <p className="p-2">Frontend developer</p>
